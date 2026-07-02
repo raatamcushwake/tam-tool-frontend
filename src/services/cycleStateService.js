30d6aad216a8aa27beb7a3cbb38f708420e763c2
raatamcushwake <raatam.cushwake@gmail.com>
2026-07-02 12:38:41 +0530

    Add STAGES constant for cycle states

--- src/services/cycleStateService.js
+++ src/services/cycleStateService.js
@@ -1,6 +1,16 @@
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
+
+export const STAGES = {
+  DRAFT: "DRAFT",
+  SANITY_RUN: "SANITY_RUN",
+  ANALYSIS_PENDING_REVIEW: "ANALYSIS_PENDING_REVIEW",
+  ANALYSIS_PENDING_MANAGER: "ANALYSIS_PENDING_MANAGER",
+  ANALYSIS_REJECTED: "ANALYSIS_REJECTED",
+  APPROVED_FROZEN: "APPROVED_FROZEN",
+};
+
 export async function getCycleState(projectId) {
   try {
     const ref = doc(db, "projects", projectId, "cycleState", "current");
